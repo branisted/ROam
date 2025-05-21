9148fba8f2e13dc59febe88e546c82f938ea753b
Dragoș <branisted@gmail.com>
2025-05-21 17:27:27 +0300

    Modified db structure for the "posts" table

--- backend/db/db.js
+++ backend/db/db.js
@@ -36,19 +36,21 @@ db.serialize(() => {
 db.serialize(() => {
     db.run(`
         CREATE TABLE IF NOT EXISTS posts (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT NOT NULL,
-        location TEXT NOT NULL,
-        type TEXT CHECK(type IN ('hike', 'bike ride', 'urban walk', 'nature tour', 'forest trail', 'mountain climb', 'city exploration', 'river walk', 'wildlife spotting', 'historical tour', 'food tour', 'cycling', 'stargazing', 'camping', 'other')) DEFAULT 'other',
-        difficulty TEXT CHECK(difficulty IN ('easy', 'moderate', 'hard')) DEFAULT 'easy',
-        estimated_duration TEXT NOT NULL,
-        photo BLOB,
-        description TEXT NOT NULL,
-        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-        author_id INTEGER NOT NULL,
-        likes INTEGER DEFAULT 0,
-        FOREIGN KEY (author_id) REFERENCES users(id)
-        )
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            location TEXT NOT NULL,
+            type TEXT CHECK(type IN ('hike', 'bike ride', 'urban walk', 'nature tour', 'forest trail', 'mountain climb', 'city exploration', 'river walk', 'wildlife spotting', 'historical tour', 'food tour', 'cycling', 'stargazing', 'camping', 'other')) DEFAULT 'other',
+            difficulty TEXT CHECK(difficulty IN ('easy', 'moderate', 'hard')) DEFAULT 'easy',
+            estimated_duration TEXT NOT NULL,
+            photo BLOB,
+            description TEXT NOT NULL,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            author_id INTEGER NOT NULL,
+            likes INTEGER DEFAULT 0,
+            is_joinable BOOLEAN DEFAULT 0,
+            max_participants INTEGER DEFAULT NULL,
+            FOREIGN KEY (author_id) REFERENCES users(id)
+            )
     `);
 });
 
