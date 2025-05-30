17fb7b1802d5c315281a09b7b51273401caacf79
Dragoș <branisted@gmail.com>
2025-05-30 04:35:41 +0300

    Added cancellation button on edit adventures page

--- backend/src/features/posts/posts.routes.js
+++ backend/src/features/posts/posts.routes.js
@@ -13,6 +13,7 @@ router.put('/:id', (req, res, next) => postsController.updatePost(req, res, next
 router.delete('/:id', (req, res, next) => postsController.deletePost(req, res, next));
 router.put('/:id/complete', requireAuth, (req, res, next) => postsController.markCompleted(req, res, next));
 router.put('/:id/uncomplete', requireAuth, (req, res, next) => postsController.markUncompleted(req, res, next));
+router.put('/:id/cancel', requireAuth, (req, res, next) => postsController.markCancelled(req, res, next));
 router.post('/:id/join', requireAuth, (req, res, next) => postsController.joinAdventure(req, res, next));
 router.post('/:id/unjoin', requireAuth, (req, res, next) => postsController.unjoinAdventure(req, res, next));
 router.get('/:id/participants', requireAuth, (req, res, next) => postsController.getParticipants(req, res, next));
